a84654ae0e511ba2951b7280c433bf56978741f1
Javier <javier.ibanez14@gmail.com>
2025-02-18 16:22:39 +0100

    Ejercicio5.3

--- exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodyPage.xaml.cs
+++ exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodyPage.xaml.cs
@@ -1,7 +1,19 @@
 namespace Astronomy.Pages;
 
+[QueryProperty(nameof(AstroName), "astroName")]
 public partial class AstronomicalBodyPage : ContentPage
 {
+    private string astroName;
+
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+            UpdateAstroBodyUI(astroName); // Actualiza la interfaz cuando se recibe el valor
+        }
+    }
 
     public AstronomicalBodyPage()
     {
@@ -10,6 +22,9 @@ public partial class AstronomicalBodyPage : ContentPage
 
     void UpdateAstroBodyUI(string astroName)
     {
+        if (string.IsNullOrEmpty(astroName))
+            return;
+
         AstronomicalBody body = FindAstroData(astroName);
 
         Title = body.Name;
@@ -29,7 +44,7 @@ public partial class AstronomicalBodyPage : ContentPage
             "earth" => SolarSystemData.Earth,
             "moon" => SolarSystemData.Moon,
             "sun" => SolarSystemData.Sun,
-            _ => throw new ArgumentException()
+            _ => throw new ArgumentException($"Invalid astronomical body: {astronomicalBodyName}")
         };
     }
 }
